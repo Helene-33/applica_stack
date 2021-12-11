e8c3bf09cbb6a7f6a8c79c3467ba0c28a702c863
Michael Lyon <lyoncodes@gmail.com>
2021-12-10 16:49:40 -0800

    deploy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.resolve(__dirname, "./group3-app/build")));
 
 app.use('/helloworld', helloWorldAPI);
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 5000;
 
 // Global handler
 app.get("*", function (request, response) {
